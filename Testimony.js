cd1fc56ca08fda096cd4d486b93683d452002893
chris ville <113802404+Chrisville1987@users.noreply.github.com>
2023-04-26 05:10:35 -0700

    Add files via upload

---
+++ Testimony.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Travel.css'
+
+const Testimony = () => {
+  return (
+    <div>
+        <div><img src="./images/logos.png" alt=''className='axon'></img></div>
+        <div><p className='monial'>Testimonials</p></div>
+        <div><p className='say'>What people say <br></br>about Us.</p></div>
+        <div><img src="./images/spot.png" alt='' className='spot'></img></div>
+        <div className='man'><img src="./images/man.png" alt='' style={{zIndex:'3'}}></img></div>
+        <div className='windows'><p style={{zIndex:'3'}}>“On the Windows talking painted pasture yet its<br></br> express parties use. Sure last upon he same as <br></br> knew next. Of believed or diverted no.”</p></div>
+        <div><img src="./images/card.png" alt='' className='card'></img></div>
+        <div><p className='mike'>Mike taylor</p></div>
+        <div><p className='lahore'> Lahore, Pakistan</p></div>
+        <div><p className='chris'>Chris Thomas</p></div>
+        <div><p className='ceo'>CEO of Red Button</p></div>
+    </div>
+  )
+}
+
+export default Testimony
